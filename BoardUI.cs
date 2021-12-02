84fd2091f5a116cac3642d21d11f5179769aa160
willc <willc@DESKTOP-B0JJU71>
2021-12-02 18:49:41 +0000

    Add project files.

---
+++ BoardUI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess_Engine_Project
+{
+    public partial class BoardUI : Form
+    {
+        public BoardUI()
+        {
+            InitializeComponent();
+        }
+
+        private void BoardUI_Load(object sender, EventArgs e)
+        {
+			this.CenterToScreen();
+			drawBoard();
+        }
+
+		public void drawBoard()
+		{
+			Bitmap board = new Bitmap(800, 800);
+			Graphics g = Graphics.FromImage(board);
+
+			for (int file = 0; file < 8; file++)
+			{
+				for (int rank = 0; rank < 8; rank++)
+				{
+					bool isLightSquare = (file + rank) % 2 == 1;
+					if (isLightSquare)
+					{
+						//filling in light squares
+						g.FillRectangle(Brushes.Wheat, file * 100, rank * 100, 100, 100);
+					}
+					else
+					{
+						//filling in dark squares 
+						g.FillRectangle(Brushes.Sienna, file * 100, rank * 100, 100, 100);
+					}
+				}
+			}
+			boardImageBox.Image = board;
+		}
+
+		public void loadPieces() 
+		{
+
+		}
+	}
+}
---
+++ Piece.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Engine_Project
+{
+    class Piece
+    {
+        public const int empty = 0;
+        public const int king = 1;
+        public const int pawn = 2;
+        public const int knight = 3;
+        public const int bishop = 4;
+        public const int rook = 5;
+        public const int queen = 6;
+        public const int blockerPiece = 7;       
+
+        public const int white = 8;
+        public const int black = 16;
+
+
+
+    }
+}
