827f15d0c95f292fdd2e54ce2eb7795068689292
unknown <wc104@canterbury.ac.uk>
2022-02-25 14:44:10 +0000

    Correctly split Project into 3 individual projects > One for backend engine code > One for handling the GUI and displaying info to the user > One for testing both the engine and the GUI
    +Included additions to unit tests, mentioned in previous commit

---
+++ Chess_Project_Tests/GUI_Tests.cs
@@ -0,0 +1,63 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chess_Project_Tests
+{
+    [TestClass]
+    public class GUI_Tests
+    {
+        ///  <summary>
+        ///  Checks board is rendered correctly
+        ///  </summary>
+        [TestMethod]
+        public void Board_Rendered()
+        {
+            //  arrange
+
+            //  act
+
+            //  assert
+
+        }
+        ///  <summary>
+        ///  Checks pieces loaded correctly according to FEN
+        ///  </summary>
+        [TestMethod]
+        public void Pieces_Loaded_Correctly()
+        {
+            //  arrange
+
+            //  act
+
+            //  assert
+
+        }
+        ///  <summary>
+        ///  Checks all pieces display correct valid moves to users
+        ///  Given a good position to check all possibilities: ie. en passant, in-check, pinned pieces etc
+        ///  </summary>
+        [TestMethod]
+        public void Piece_Movement_Validation()
+        {
+            //  arrange
+
+            //  act
+
+            //  assert
+
+        }
+        ///  <summary>
+        ///  tests GUI buttons to ensure they function correctly
+        ///  i.e next, back and reset buttons
+        ///  </summary>
+        [TestMethod]
+        public void GUI_Buttons_Test()
+        {
+            //  arrange
+
+            //  act
+
+            //  assert
+
+        }
+    }
+}
