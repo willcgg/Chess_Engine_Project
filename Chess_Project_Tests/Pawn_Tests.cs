22ecd067a07e957056d8619e2566692e5c1bcd66
William Castleman <wc104@canterbury.ac.uk>
2022-03-03 21:13:13 +0000

    <mod> Altered Piece_Testing.cs structure
    + Added new class to deal with all pawn movement testing.
    + Added methods to test aspects of pawns movement.
    + Piece_Testing.cs will now act as hub calling all individual piece tests.

---
+++ Chess_Project_Tests/Pawn_Tests.cs
@@ -0,0 +1,55 @@
+﻿namespace Chess_Project_Tests
+{
+    [TestClass]
+    class Pawn_Tests
+    {
+
+        Board board;
+
+        [TestMethod]
+        public void Pawn_Movement_Valid() {
+
+            //  arrange
+            board = new Board();
+            PopulateBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+
+            //  act
+            Make_Move("a2", "a4");      // MakeMove takes two parameters, current board position and desired position
+            
+
+            //  assert
+            Assert.True(board[81] == Peice.PeiceType.empty);        // Checks piece has moved
+            Assert.True(board[71] == Peice.PeiceType.pawn);         // 
+            
+        }
+
+        public void Pawn_Movement_En_Passant() {
+
+            //  arrange
+            board = new Board();
+            PopulateBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+
+            //  act
+            Make_Move("a7", "a6");      // Setting up en-passant
+            Make_Move("a4", "a5");      //
+            Make_Move("b7", "b5");      //
+            Make_Move("a5", "b6");      // Taking en-passant
+
+            //  assert
+            Assert.True(board[71] == Peice.PeiceType.empty);                                    // Checking piece has moved
+            Assert.True(board[82] == Peice.PeiceType.pawn && Peice.PeiceColour.white);          // 
+            Assert.True(board[72] == Peice.PeiceType.empty);                                    // Checks black piece was correctly removed from the board
+
+        }
+
+        public void Pawn_Movement_in_Check() {
+
+        }
+
+        public void Pawn_Movement_Pinned() {
+            
+        }
+
+
+    }
+}
--- Chess_Project_Tests/Piece_Testing.cs
+++ Chess_Project_Tests/Piece_Testing.cs
@@ -1,25 +1,30 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chess_Engine;
 
 namespace Chess_Project_Tests
 {
     /// <summary>
-    /// Class to fully check all the pieces function correctly
+    /// Class to call all peice movement tests
     /// </summary>
     [TestClass]
     public class Piece_Testing
     {
         /// <summary>
         /// <para>Checks all possible valid pawn moves</para>
-        /// <para>E.g. Given a good position to check all possibilities: en passant, in-check, pinned pieces etc...</para>
+        /// <para>Individually calls all pawn movement tests. Each test is given a good board position to
+        /// check all possibilities. E.g. en passant, in-check, pinned pieces etc..</para>
         /// </summary>
         [TestMethod]
         public void Pawn_Movement() {
-            //  arrange
 
-            //  act
+            // Arrange
+            Pawn_Tests pt = new Pawn_Tests();
 
-            //  assert
-
+            // Act & Assert
+            pt.Pawn_Movement_Valid();
+            pt.Pawn_Movement_En_Passant();
+            pt.Pawn_Movement_in_Check();
+            pt.Pawn_Movement_Pinned();
         }
         /// <summary>
         /// <para>Checks all possible rook movements</para>
